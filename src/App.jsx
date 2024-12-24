940398a00bd036a33ce10b0bd4b3e8e745449496
Suman Saurabh <sumsau2022@gmail.com>
2024-12-24 23:45:18 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,6 @@ function App() {
   const [phoneNo, setPhoneNo] = useState('')
   const [profilePic, setProfilePic] = useState('')
 
-  
-
 
   useEffect(() => {
     axios.get('https://randomuser.me/api/?page=1&results=1')
@@ -38,24 +36,24 @@ function App() {
           setPhoneNo(data.phone)
           setProfilePic(data.picture.large)
         })
-    }, 2000)
+    }, 3000)
     
   },[])
 
   return (
     <div className='w-full min-h-screen place-content-center bg-gradient-to-br dark:from-slate-950 dark:via-slate-800 dark:to-slate-700 dark:text-white text-slate-800 from-blue-400 via-blue-200 to-blue-50'>
-      <div className='flex flex-row max-w-[400px] h-[155px] mx-auto p-3 bg-sky-200 dark:bg-sky-900 rounded-2xl justify-between text-lg font-semibold border-2 dark:border-sky-100 border-sky-500 shadow-slate-700 dark:shadow-slate-600 hover:shadow-2xl shadow-xl transition-all ease-out duration-700 hover:scale-105'
+      <div className='flex flex-row max-w-[400px] h-[155px] mx-auto p-3 bg-gradient-to-r dark:from-indigo-600 from-indigo-300 dark:via-purple-600 via-purple-300 dark:to-pink-600 to-pink-300 rounded-2xl justify-between text-lg font-semibold border-2 dark:border-sky-100 border-sky-500 shadow-slate-700 dark:shadow-slate-600 hover:shadow-2xl shadow-xl transition-all ease-out duration-700 hover:scale-105'
       title='Click to Change User'
       >
         <div>
           <img src={profilePic} alt={firstName} 
-          className='rounded-2xl'
+          className='rounded-2xl ring-2 ring-sky-500 dark:ring-sky-100'
           />
         </div>
         <div className='text-right py-4 transition-all duration-0'>
-          <h2 className='font-bold text-xl'>{firstName} {lastName}</h2>
-          <h2>{gender.toUpperCase()}</h2>
-          <h2>{phoneNo}</h2>
+          <h2 className='font-bold text-2xl'>{firstName} {lastName}</h2>
+          <h2 className='text-sm'>{gender.toUpperCase()}</h2>
+          <h2 className='text-sm'>{phoneNo}</h2>
         </div>
       </div>
       <div className='flex justify-center mt-36'>
