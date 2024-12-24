f30a39dc621191e5c9ac7ed80b7ae8f0bb63d05c
Suman Saurabh <sumsau2022@gmail.com>
2024-12-24 23:28:58 +0530

    Task done

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+function App() {
+
+  const toggleTheme = () => {
+    document.documentElement.classList.toggle('dark')
+  }
+
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [gender, setGender] = useState('')
+  const [phoneNo, setPhoneNo] = useState('')
+  const [profilePic, setProfilePic] = useState('')
+
+  
+
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api/?page=1&results=1')
+      .then((res) => res.data.results[0])
+      .then((data) => {
+        setFirstName(data.name.first)
+        setLastName(data.name.last)
+        setGender(data.gender)
+        setPhoneNo(data.phone)
+        setProfilePic(data.picture.large)
+      })
+
+    setInterval(() => {
+      axios.get('https://randomuser.me/api/?page=1&results=1')
+        .then((res) => res.data.results[0])
+        .then((data) => {
+          setFirstName(data.name.first)
+          setLastName(data.name.last)
+          setGender(data.gender)
+          setPhoneNo(data.phone)
+          setProfilePic(data.picture.large)
+        })
+    }, 2000)
+    
+  },[])
+
+  return (
+    <div className='w-full min-h-screen place-content-center bg-gradient-to-br dark:from-slate-950 dark:via-slate-800 dark:to-slate-700 dark:text-white text-slate-800 from-blue-400 via-blue-200 to-blue-50'>
+      <div className='flex flex-row max-w-[400px] h-[155px] mx-auto p-3 bg-sky-200 dark:bg-sky-900 rounded-2xl justify-between text-lg font-semibold border-2 dark:border-sky-100 border-sky-500 shadow-slate-700 dark:shadow-slate-600 hover:shadow-2xl shadow-xl transition-all ease-out duration-700 hover:scale-105'
+      title='Click to Change User'
+      >
+        <div>
+          <img src={profilePic} alt={firstName} 
+          className='rounded-2xl'
+          />
+        </div>
+        <div className='text-right py-4 transition-all duration-0'>
+          <h2 className='font-bold text-xl'>{firstName} {lastName}</h2>
+          <h2>{gender.toUpperCase()}</h2>
+          <h2>{phoneNo}</h2>
+        </div>
+      </div>
+      <div className='flex justify-center mt-36'>
+        <button
+        onClick={toggleTheme}
+        className='font-semibold py-2 px-4 rounded dark:bg-slate-700 bg-blue-300 dark:only:hover:bg-slate-600 hover:bg-blue-400 animate-bounce'
+        >Toggle Me</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
